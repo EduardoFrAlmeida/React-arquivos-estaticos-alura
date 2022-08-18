0c63a2a46a85bfa0a4b3e8857f1308bf197c959f
Eduardo Almeida <eduardofralmeida745@gmail.com>
2022-08-18 16:01:34 -0300

    modulo-4

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,13 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
   return (
     <main>
       <nav className={styles.menu}>
@@ -18,6 +22,10 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
       </section>
     </main>
   )
